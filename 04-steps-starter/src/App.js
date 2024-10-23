f47b62fdfdd41aeb4eab016205f4ab963dec6a00
SIVADA007 <sivamahatma007@gmail.com>
2024-10-24 01:20:32 +0530

    newone

---
+++ 04-steps-starter/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function App(props) {
+  
+
+  return (
+    
+      <div className='steps'>
+          <div className="numbers">
+            <div className="">1</div>
+            <div className="">2</div>
+            <div className="">3.1</div>
+          </div>
+
+          <p className="message">Hello</p>
+
+          <div className="buttons">
+            <button style={{backgroundColor:'#7050f2', color:'#ffff'}}>Previous</button>
+            <button>Next</button>
+          </div>
+      </div>
+    
+  )
+}
